f0c240c69a91b396c0d004349ae595eccd6d5610
Saikhana <37629199+Saikhana@users.noreply.github.com>
2022-05-17 23:58:40 +0200

    added session id

--- src/test/java/GetDesiredCapabilitySession.java
+++ src/test/java/GetDesiredCapabilitySession.java
@@ -14,8 +14,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
- 
-public class getCookie {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -27,20 +25,12 @@ public class getCookie {
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 30);
     }
-    public static void main(String args[]) {
-        MainPage mainPage = new MainPage(this.driver);
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        
-        //For creating a new cookie we should pass the name of the cookie and its value
-        Cookie cname = new Cookie("myCookie", "12345678999");
-        this.driver.manage().addCookie(cname);
- 
-        //retrieve the cookies to view the newly added cookie
-        Set<Cookie> cookiesList =  this.driver.manage().getCookies();
-        for(Cookie getcookies :cookiesList) {
-            System.out.println(getcookies );
-        }
-        this.driver.close();
-    }
+public class GetDesiredCapabilitySession {
+
+    MainPage mainPage = new MainPage(this.driver);
+    executor_url = this.driver.command_executor._url
+    session_id = this.driver.session_id
+
+    print this.driver.current_url
+
 }
